aa938cf944d9f34a56dafe52b7371591c7bd61e5
sajones9 <78449405+sajones9@users.noreply.github.com>
2021-02-02 22:46:30 -0800

    Full pipeline test with new tst msg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('daddies cummies!'));
+app.get('/', (req, res) => res.send('pls dont pay attention to my last example msg idk what im doing'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
